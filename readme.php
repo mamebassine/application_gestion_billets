8d0950f5d899dbd3d9ca852064ff910f0599101e
mamebassine <mamebassine06@gmail.com>
2024-03-08 22:00:58 +0000

    Ceci est ma page de connection a la base de donnée que je inclure dans tout les pages.

---
+++ readme.php
@@ -0,0 +1,68 @@
+Nom du projet
+Projet de Réservation de Billets de Voyage
+
+Description
+Le projet de réservation de billets de voyage est une application web développée pour la plateforme Windows.
+ Cette solution permet aux utilisateurs de rechercher, réserver et gérer leurs billets de voyage de manière conviviale.
+L'application utilise les langages HTML, CSS, PHP, ainsi que la base de données MySQL via XAMPP.
+La conception du système a été réalisée en suivant la méthode MERISE.
+
+Fonctionnalités
+Recherche de Billets : Les utilisateurs peuvent rechercher des billets en fonction de différents critères tels que la destination, la date, etc.
+Réservation de Billets : Les utilisateurs peuvent réserver des billets pour les trajets sélectionnés avec une interface conviviale.
+Gestion des Réservations : Un tableau de bord permet aux utilisateurs de gérer leurs réservations, y compris la modification et l'annulation.
+Détails des Voyages : Les détails complets des voyages sont affichés,la création, l'affichage, la mise à jour et la suppression de billets
+
+Configuration et Installation
+Prérequis
+Assurez-vous d'avoir installé les éléments suivants sur votre machine Windows :
+XAMPP pour le serveur Apache et MySQL.
+Assurez-vous d'avoir PHP 8.2.10, 10.4.28-MariaDB et Apache/2.4.56  (ou un serveur web équivalent) installés sur votre machine.,4.,
+Configuration de la Base de Données
+Apres installation de XAMPP et démarrez les services Apache et MySQL.
+Configurez les paramètres de base de données dans le fichier config.php.
+
+Configuration du Projet
+Clonez le dépôt : git clone https://github.com/votre-nom/projet-reservation-billets.git
+Accédez au répertoire du projet : cd projet-reservation-billets
+Lancement de l'Application
+Installez les dépendances : npm install
+Démarrez le serveur : npm start
+Accès à l'Application
+Ouvrez votre navigateur et accédez à http://localhost/application_gestion_billets.
+
+Utilisation
+Recherche et Réservation :
+Utilisez la barre de recherche pour trouver des billets selon vos critères.
+Sélectionnez un trajet et procédez à la réservation.
+Gestion des Réservations :
+Consultez et gérez vos réservations depuis le tableau de bord.
+Contribution au Projet
+Si vous souhaitez contribuer au projet, suivez ces étapes :
+
+Contribution au Projet
+Fork du Projet 
+Cliquez sur "Fork" sur la page du projet GitHub pour créer votre propre copie du projet.
+Clonage du Projet Localement :
+Clonez votre fork sur votre machine avec git clone https://github.com/votre-nom/projet-reservation-billets.git.
+Création d'une Nouvelle Branche :
+Créez une nouvelle branche pour votre contribution avec git checkout -b feature/nom-de-la-fonctionnalite.
+Faites vos Modifications et Commit :
+Effectuez les changements nécessaires, ajoutez-les à l'index avec git add ., puis commit avec git commit -m 'Ajouter une nouvelle fonctionnalité'.
+Pushez vers Votre Branche :
+Pushez vos modifications sur GitHub avec git push origin feature/nom-de-la-fonctionnalite.
+Création d'une Pull Request :
+Sur GitHub, cliquez sur "New pull request".
+Sélectionnez votre branche.
+Ajoutez une description détaillée.
+Cliquez sur "Create pull request" pour soumettre votre contributionull request.
+
+Actrice
+MAME BASSINE NIANG
+
+Licence
+Aucun
+
+
+
+
